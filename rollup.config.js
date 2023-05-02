6b7686a74b9da0f9348327177b9e9cab2ab03a97
viacheslav <bviacheslav.12@gmail.com>
2023-05-03 01:29:44 +0300

    Moved to new @rollup depts and used tercer to uglify script in browser, apply babel only in browser bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,32 @@
-import resolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
-import babel from "rollup-plugin-babel";
 import pkg from "./package.json";
+import { terser } from "rollup-plugin-terser";
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs'
+import babel from '@rollup/plugin-babel'
 
 export default [
     {
         input: "src/index.js",
-        output: {
-            name: "tieder",
-            file: pkg.browser,
-            format: "umd",
-        },
+        output: [
+            { name: "tieder", file: pkg.browser, format: "umd" },
+            { file: pkg.module, format: "es" }
+        ],
         plugins: [
-            resolve(),
+            nodeResolve({ browser: true }),
             commonjs(),
             babel({
-                exclude: ["node_modules/**"],
+                exclude: ["node_modules/**"]
             }),
+            terser()
         ],
     },
     {
         input: "src/index.js",
         output: [
-            { file: pkg.main, format: "cjs" },
-            { file: pkg.module, format: "es" },
+            { file: pkg.main, format: "cjs" }
         ],
         plugins: [
-            babel({
-                exclude: ["node_modules/**"],
-            }),
-        ],
+            nodeResolve()
+        ]
     },
 ];
